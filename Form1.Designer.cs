2dbf6d83e6b5f3e0acb683df2de341fd48391c1c
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-03-08 22:02:27 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ADICIONARELIMPAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btonAd = new System.Windows.Forms.Button();
+            this.btnRm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite seu Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(176, 61);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(248, 20);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(183, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // btonAd
+            // 
+            this.btonAd.Location = new System.Drawing.Point(183, 198);
+            this.btonAd.Name = "btonAd";
+            this.btonAd.Size = new System.Drawing.Size(75, 23);
+            this.btonAd.TabIndex = 3;
+            this.btonAd.Text = "ADICIONAR";
+            this.btonAd.UseVisualStyleBackColor = true;
+            this.btonAd.Click += new System.EventHandler(this.btonAd_Click);
+            // 
+            // btnRm
+            // 
+            this.btnRm.Location = new System.Drawing.Point(380, 198);
+            this.btnRm.Name = "btnRm";
+            this.btnRm.Size = new System.Drawing.Size(75, 23);
+            this.btnRm.TabIndex = 4;
+            this.btnRm.Text = "REMOVER";
+            this.btnRm.UseVisualStyleBackColor = true;
+            this.btnRm.Click += new System.EventHandler(this.btnRm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRm);
+            this.Controls.Add(this.btonAd);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btonAd;
+        private System.Windows.Forms.Button btnRm;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADICIONARELIMPAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btonAd_Click(object sender, EventArgs e)
+        {
+            if (textBoxNome.TextLength== 0)
+                MessageBox.Show ("Por favor digite algo!", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textBoxNome.Focus();
+            { 
+            comboBox1.Items.Add(textBoxNome.Text);
+            textBoxNome.Clear();
+            textBoxNome.Focus();
+            }
+        }
+
+        private void btnRm_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+                MessageBox.Show("Por favor, selecione um item", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textBoxNome.Focus();
+            { 
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                comboBox1.SelectedIndex = 0;    
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        //    comboBox1.SelectedIndex = 0;
+        }
+    }
+}
